0dc8eec75cd529a17f7047be01e02dea001f6ec4
Nitheshwaran <vignesh@dreaminfinity.in>
2024-09-28 22:24:51 +0530

    Revert "responsive corrections"
    This reverts commit 84e1e6d0ce813295020173ba7950966c7145e725.

--- Client/src/Pages/login/LoginScreen.jsx
+++ Client/src/Pages/login/LoginScreen.jsx
@@ -1,3 +1,4 @@
+// src/screens/LoginScreen.jsx
 import "./LoginScreen.css"; // Import the CSS file
 import "../../style/shorthand.css";
 import Snackbar from "@mui/material/Snackbar";
@@ -39,6 +40,7 @@ const LoginScreen = () => {
       console.log("Login response:", data); // Log response for debugging
 
       if (response.ok) {
+        // Call login function from context
         login(data.token, { username: data.username, email: data.email });
         console.log("Token stored in localStorage:", data.token);
         console.log("Navigating to home...");
@@ -53,7 +55,7 @@ const LoginScreen = () => {
   };
 
   return (
-    <div className="flex flex-col lg:flex-row h-screen">
+    <div className="flex h-screen">
       <Snackbar
         open={open}
         autoHideDuration={1000}
@@ -61,18 +63,18 @@ const LoginScreen = () => {
         message="Login failed. Please check your credentials."
       />
 
-      {/* Onboarding Left */}
-      <div className="flex-1 hidden lg:flex leftContainer">
+      {/* Onboarding Left  */}
+      <div className="flex-1 leftContainer">
         <Onboardleft />
       </div>
 
-      <div className="flex-1 bg-white p-4 lg:p-8 flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center w-full gap-6 lg:gap-8">
+      <div className="flex-1 bg-white p-8 flex">
+        <div className="flex flex-col justify-center items-center w-full gap-8">
           <div className="themeClr text-2xl uppercase font-bold">logo</div>
           <div>
             <GloginBtn /> {/* Google Login Button */}
           </div>
-          <span className="mx-2 text-lg">or</span>
+          <span className="mx-2">or</span>
 
           <div>
             <TextField
@@ -81,7 +83,7 @@ const LoginScreen = () => {
               variant="outlined"
               value={email}
               onChange={(e) => setEmail(e.target.value)} // Update state
-              sx={{ width: "280px", maxWidth: "100%" }} // Responsive width
+              sx={{ width: "350px" }}
             />
           </div>
           <div>
@@ -92,13 +94,13 @@ const LoginScreen = () => {
               variant="outlined"
               value={password}
               onChange={(e) => setPassword(e.target.value)} // Update state
-              sx={{ width: "280px", maxWidth: "100%" }} // Responsive width
+              sx={{ width: "350px" }}
             />
             <div className="forgetPassword flex justify-between items-center font-bold">
               <p>
                 Forgot your <span className="themeClr">Password?</span>
               </p>
-              <Link to="/signup" className="text-blue-500 hover:underline">or Register</Link>
+              <Link to="/signup">or Register</Link>
             </div>
           </div>
 
@@ -108,7 +110,7 @@ const LoginScreen = () => {
               sx={{
                 backgroundColor: "#6C63FF",
                 borderRadius: "20px",
-                width: "240px",
+                width: "260px",
               }}
               onClick={handleLogin} // Call handleLogin on button click
             >
--- Client/src/component/Onboarding/Onboardleft.jsx
+++ Client/src/component/Onboarding/Onboardleft.jsx
@@ -5,37 +5,45 @@ import Login3 from "../../assets/Login3.svg";
 
 const Onboardleft = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-full">
-      <div className="cw mt-6 pl-10 text-3xl uppercase font-bold hover:text-rose-50">
-        logo
-      </div>
-      <div className="flex flex-col items-center justify-center text-lg uppercase font-bold gap-8 mt-8 w-full max-w-md"> {/* Limit the width of the container */}
-        {/* First Section */}
-        <div className="flex items-center justify-center w-full">
-          <img className="im-sz w-32 h-32 md:w-40 md:h-40" src={Login1} alt="Pay Online" />
-          <div className="flex flex-col text-center ml-4">
-            <span>Pay Online,</span>
-            <span className="cw">with a simple tap!</span>
+    <>
+      
+        <div className="flex flex-row">
+          <div className="cw mt-6 pl-10 text-2xl uppercase font-bold hover:text-rose-50">
+            logo
           </div>
         </div>
-        {/* Second Section */}
-        <div className="flex items-center justify-center w-full">
-          <div className="flex flex-col text-center">
-            <span>Unlimited</span>
-            <span className="cw">customization's</span>
-          </div>
-          <img className="im-sz w-32 h-32 md:w-40 md:h-40 ml-4" src={Login2} alt="Unlimited Customization" />
-        </div>
-        {/* Third Section */}
-        <div className="flex items-center justify-center w-full">
-          <img className="im-sz w-32 h-32 md:w-40 md:h-40" src={Login3} alt="Pay Online" />
-          <div className="flex flex-col text-center ml-4">
-            <span>Pay Online,</span>
-            <span className="cw">with a simple tap!</span>
+        <div className="flex flex-row justify-center">
+          <div className="text-2xl flex items-center justify-center uppercase font-bold ">
+            <img className="im-sz" src={Login1} alt="" srcset="" />
+            <div className="flex flex-col">
+              {" "}
+              <span>Pay Online,</span>
+              <span className="cw">with a simple tap!</span>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+        <div className="flex flex-row justify-center">
+          <div className=" text-2xl flex items-center justify-center uppercase font-bold ">
+            <div className="flex flex-col">
+              {" "}
+              <span>Unlimited</span>
+              <span className="cw">customization's</span>
+            </div>
+            <img className="im-sz" src={Login2} alt="" srcset="" />
+          </div>
+        </div>
+        <div className="flex flex-row justify-center">
+          <div className="text-2xl flex items-center justify-center uppercase font-bold ">
+            <img className="im-sz" src={Login3} alt="" srcset="" />
+            <div className=" flex flex-col">
+              {" "}
+              <span>Pay Online,</span>
+              <span className="cw">with a simple tap!</span>
+            </div>
+          </div>
+        </div>
+      
+    </>
   );
 };
 
