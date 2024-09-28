84e1e6d0ce813295020173ba7950966c7145e725
Nitheshwaran <vignesh@dreaminfinity.in>
2024-09-28 22:21:37 +0530

    responsive corrections

--- Client/src/Pages/login/LoginScreen.jsx
+++ Client/src/Pages/login/LoginScreen.jsx
@@ -1,4 +1,3 @@
-// src/screens/LoginScreen.jsx
 import "./LoginScreen.css"; // Import the CSS file
 import "../../style/shorthand.css";
 import Snackbar from "@mui/material/Snackbar";
@@ -40,7 +39,6 @@ const LoginScreen = () => {
       console.log("Login response:", data); // Log response for debugging
 
       if (response.ok) {
-        // Call login function from context
         login(data.token, { username: data.username, email: data.email });
         console.log("Token stored in localStorage:", data.token);
         console.log("Navigating to home...");
@@ -55,7 +53,7 @@ const LoginScreen = () => {
   };
 
   return (
-    <div className="flex h-screen">
+    <div className="flex flex-col lg:flex-row h-screen">
       <Snackbar
         open={open}
         autoHideDuration={1000}
@@ -63,18 +61,18 @@ const LoginScreen = () => {
         message="Login failed. Please check your credentials."
       />
 
-      {/* Onboarding Left  */}
-      <div className="flex-1 leftContainer">
+      {/* Onboarding Left */}
+      <div className="flex-1 hidden lg:flex leftContainer">
         <Onboardleft />
       </div>
 
-      <div className="flex-1 bg-white p-8 flex">
-        <div className="flex flex-col justify-center items-center w-full gap-8">
+      <div className="flex-1 bg-white p-4 lg:p-8 flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center w-full gap-6 lg:gap-8">
           <div className="themeClr text-2xl uppercase font-bold">logo</div>
           <div>
             <GloginBtn /> {/* Google Login Button */}
           </div>
-          <span className="mx-2">or</span>
+          <span className="mx-2 text-lg">or</span>
 
           <div>
             <TextField
@@ -83,7 +81,7 @@ const LoginScreen = () => {
               variant="outlined"
               value={email}
               onChange={(e) => setEmail(e.target.value)} // Update state
-              sx={{ width: "350px" }}
+              sx={{ width: "280px", maxWidth: "100%" }} // Responsive width
             />
           </div>
           <div>
@@ -94,13 +92,13 @@ const LoginScreen = () => {
               variant="outlined"
               value={password}
               onChange={(e) => setPassword(e.target.value)} // Update state
-              sx={{ width: "350px" }}
+              sx={{ width: "280px", maxWidth: "100%" }} // Responsive width
             />
             <div className="forgetPassword flex justify-between items-center font-bold">
               <p>
                 Forgot your <span className="themeClr">Password?</span>
               </p>
-              <Link to="/signup">or Register</Link>
+              <Link to="/signup" className="text-blue-500 hover:underline">or Register</Link>
             </div>
           </div>
 
@@ -110,7 +108,7 @@ const LoginScreen = () => {
               sx={{
                 backgroundColor: "#6C63FF",
                 borderRadius: "20px",
-                width: "260px",
+                width: "240px",
               }}
               onClick={handleLogin} // Call handleLogin on button click
             >
